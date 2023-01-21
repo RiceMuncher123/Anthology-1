c5e7fb8dab18c17756b0fe0b06d4907babf34c5e
Daniel-Tann <340883792@gapps.yrdsb.ca>
2023-01-20 19:22:37 -0500

    bhjlopkmbvjyu

---
+++ main.js
@@ -0,0 +1,17 @@
+let reflection3 = document.getElementById('reflection3Text');
+document.getElementById('button3').onclick = function(){
+  reflection3.classList.toggle('fade');
+}
+
+let reflection1 = document.getElementById('reflection1Text');
+document.getElementById('button1').onclick = function(){
+  reflection1.classList.toggle('fade');
+}
+let reflection2 = document.getElementById('reflection2Text');
+document.getElementById('button2').onclick = function(){
+  reflection2.classList.toggle('fade');
+}
+let unitReflection = document.getElementById('unitReflection');
+document.getElementById('button4').onclick = function(){
+  unitReflection.classList.toggle('fade');
+}
